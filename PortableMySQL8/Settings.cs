b7e813a2ff5d80b180f6ea7d0aef71c34c99249a
mewtwo0641 <none@none.net>
2023-04-07 22:36:43 -0500

    Some white space and tabs cleanup in Settings.cs

--- PortableMySQL8/Settings.cs
+++ PortableMySQL8/Settings.cs
@@ -11,72 +11,72 @@ namespace PortableMySQL8
 {
     public class SettingsHelper
     {
-		private void CreateSettingsIfNotExists(string path, Settings settings)
-		{
-			if (!File.Exists(path))
-			{
-				try
-				{
-					if (settings == null)
-						settings = new Settings();
+        private void CreateSettingsIfNotExists(string path, Settings settings)
+        {
+            if (!File.Exists(path))
+            {
+                try
+                {
+                    if (settings == null)
+                        settings = new Settings();
 
-					SaveSettings(path, settings);
-				}
+                    SaveSettings(path, settings);
+                }
 
-				catch { }
-			}
-		}
+                catch { }
+            }
+        }
 
-		public Settings LoadSettings(string path, Settings settings)
-		{
-			try
-			{
-				CreateSettingsIfNotExists(path, settings);
+        public Settings LoadSettings(string path, Settings settings)
+        {
+            try
+            {
+                CreateSettingsIfNotExists(path, settings);
 
-				if (File.Exists(path))
-					settings = JsonConvert.DeserializeObject<Settings>(File.ReadAllText(path));
+                if (File.Exists(path))
+                    settings = JsonConvert.DeserializeObject<Settings>(File.ReadAllText(path));
 
-				return settings;
-			}
+                return settings;
+            }
 
-			catch (Exception ex)
-			{
-				Console.WriteLine(ex.ToString());
-				return null;
-			}
-		}
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+                return null;
+            }
+        }
 
-		public bool SaveSettings(string path, Settings settings)
-		{
-			try
-			{
-				File.WriteAllText(path, JsonConvert.SerializeObject(settings, Formatting.Indented));
-				return true;
-			}
+        public bool SaveSettings(string path, Settings settings)
+        {
+            try
+            {
+                File.WriteAllText(path, JsonConvert.SerializeObject(settings, Formatting.Indented));
+                return true;
+            }
 
-			catch
-			{
-				return false;
-			}
-		}
-	}
+            catch
+            {
+                return false;
+            }
+        }
+    }
 
-	public class Settings
+    public class Settings
     {
-		public SettingsGeneral General { get; set; } = new SettingsGeneral();
-		public SettingsMySQL MySQL { get; set; } = new SettingsMySQL();
-	}
+        public SettingsGeneral General { get; set; } = new SettingsGeneral();
+        public SettingsMySQL MySQL { get; set; } = new SettingsMySQL();
+    }
 
-	public class SettingsGeneral
-	{
-		public Size WindowSize { get; set; } = new Size(515, 200);
-		public Point WindowLocation { get; set; } = new Point();
-	}
+    public class SettingsGeneral
+    {
+        public Size WindowSize { get; set; } = new Size(515, 200);
+        public Point WindowLocation { get; set; } = new Point();
+    }
 
-	public class SettingsMySQL
-	{
-		public string RootPass { get; set; } = String.Empty;
-		public bool SavePass { get; set; } = false;
-		public int Port { get; set; } = 3306;
-	}
+    public class SettingsMySQL
+    {
+        public string RootPass { get; set; } = String.Empty;
+        public bool SavePass { get; set; } = false;
+        public int Port { get; set; } = 3306;
+    }
 }
