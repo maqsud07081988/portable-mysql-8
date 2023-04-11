9cdbe31c47a02487c5fe25b7c6e5f44d31687bbf
mewtwo0641 <none@none.net>
2023-04-11 10:40:51 -0500

    Add license info to TabAbout.xaml.cs

--- PortableMySQL8/Version.cs
+++ PortableMySQL8/Version.cs
@@ -33,7 +33,7 @@ namespace PortableMySQL8
         public const int MAJOR   = 0;
         public const int MINOR   = 0;
         public const int RELEASE = 1;
-        public const int BUILD   = 67;
+        public const int BUILD   = 77;
 
         public static string VersionPretty
         {
